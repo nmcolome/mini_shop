1677ffd8b619987d942d46dd0d8bad49403ea082
NataliaColome <nmcolome@gmail.com>
2019-08-22 00:50:19 -0600

    Add merchant#show test, change Merchant model test & model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,2 @@
 class Merchant < ApplicationRecord
-  validates_presence_of :name
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Merchant do
-  describe 'validations' do
-    it { should validate_presence_of :name }
+  describe 'attributes' do
+    it { is_expected.to respond_to :name }
+    it { is_expected.to respond_to :address }
+    it { is_expected.to respond_to :city }
+    it { is_expected.to respond_to :state }
+    it { is_expected.to respond_to :zip }
   end
 end
