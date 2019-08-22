fe583be764723c87dae2d87d2b11b92afa665976
NataliaColome <nmcolome@gmail.com>
2019-08-22 00:34:58 -0600

    Add route, action & views for merchants#index

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant index page' do
   it "shows a list of all merchant's names" do
-    merchant = Merchant.new(name: 'Big Store')
-    merchant2 = Merchant.new(name: 'Small Store')
+    merchant = Merchant.create!(name: 'Big Store')
+    merchant2 = Merchant.create!(name: 'Small Store')
 
     visit '/merchants'
 
@@ -11,7 +11,3 @@ RSpec.describe 'Merchant index page' do
     expect(page).to have_content(merchant2.name)
   end
 end
-
-# As a visitor
-# When I visit '/merchants'
-# Then I see the name of each merchant in the system
