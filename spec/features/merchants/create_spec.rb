158804d6d43016a1cf0eb1b4c96ee9429e876a6a
NataliaColome <nmcolome@gmail.com>
2019-08-23 13:31:00 -0600

    Add route, action & view for merchant new & create

--- spec/features/merchants/create_spec.rb
+++ spec/features/merchants/create_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe 'New Merchant' do
 
       expect(current_path).to eq('/merchants/new')
 
-      fill_in 'Name', with: 'The Market'
-      fill_in 'Address', with: '1234 16th St'
-      fill_in 'City', with: 'Los Angeles'
-      fill_in 'State', with: 'CA'
-      fill_in 'Zip', with: '01010'
+      fill_in 'merchant[name]', with: 'The Market'
+      fill_in 'merchant[address]', with: '1234 16th St'
+      fill_in 'merchant[city]', with: 'Los Angeles'
+      fill_in 'merchant[state]', with: 'CA'
+      fill_in 'merchant[zip]', with: '01010'
       click_on 'Create'
 
       expect(current_path).to eq('/merchants')
@@ -21,19 +21,3 @@ RSpec.describe 'New Merchant' do
     end
   end
 end
-
-# As a visitor
-# When I visit the Merchant Index page
-# Then I see a link to create a new merchant
-# When I click this link
-# Then I am taken to '/merchants/new' where I  see a form for a new merchant
-# When I fill out the form with a new merchant's:
-# - name
-# - address
-# - city
-# - state
-# - zip
-# And I click the button to submit the form
-# Then a `POST` request is sent to '/merchants',
-# a new merchant is created,
-# and I am redirected to the Merchant Index page where I see the new Merchant listed.
