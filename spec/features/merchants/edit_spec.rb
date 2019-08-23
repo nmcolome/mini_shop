922a7ae23afcef24431dd7c451a376239845a8c5
NataliaColome <nmcolome@gmail.com>
2019-08-23 15:13:33 -0600

    Delete comments

--- spec/features/merchants/edit_spec.rb
+++ spec/features/merchants/edit_spec.rb
@@ -26,19 +26,3 @@ RSpec.describe 'Merchant update' do
     end
   end
 end
-
-# As a visitor
-# When I visit a merchant show page
-# Then I see a link to update the merchant
-# When I click the link
-# Then I am taken to '/merchants/:id/edit' where I  see a form to edit the merchant's data including:
-# - name
-# - address
-# - city
-# - state
-# - zip
-# When I fill out the form with updated information
-# And I click the button to submit the form
-# Then a `PATCH` request is sent to '/merchants/:id',
-# the merchant's info is updated,
-# and I am redirected to the Merchant's Show page where I see the merchant's updated info
