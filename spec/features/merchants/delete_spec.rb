216e5c3630e56cceca742f80888742c8061c9f21
NataliaColome <nmcolome@gmail.com>
2019-08-23 15:26:13 -0600

    Add route & action for merchant#destroy

--- spec/features/merchants/delete_spec.rb
+++ spec/features/merchants/delete_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Merchant delete' do
       merchant = Merchant.create!(name: 'Big Store', address: 'address', city: 'denver', state: 'CO', zip:'11111')
       merchant2 = Merchant.create!(name: 'Small Store')
 
-      visit "merchants/#{@merchant.id}"
+      visit "merchants/#{merchant.id}"
 
       click_on 'Delete Merchant'
 
@@ -16,11 +16,3 @@ RSpec.describe 'Merchant delete' do
     end
   end
 end
-
-# As a visitor
-# When I visit a merchant show page
-# Then I see a link to delete the merchant
-# When I click the link
-# Then a 'DELETE' request is sent to '/merchants/:id',
-# the merchant is deleted,
-# and I am redirected to the merchant index page where I no longer see this merchant
