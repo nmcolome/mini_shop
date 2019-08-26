4d13c0bab319da0a5cac7868c6d7114b5eb5f9d9
NataliaColome <nmcolome@gmail.com>
2019-08-26 17:14:41 -0600

    Add test for Merchant Items Index

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -1,20 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Items index page' do
-  it 'shows a list of items with their information' do
-    item1 = create(:item)
-    item2 = create(:item, name: 'good product', status: 'inactive')
+  before(:each) do
+    @merchant = create(:merchant)
+    @item1 = create(:item, merchant_id: @merchant.id)
+    @item2 = create(:item, name: 'good product', status: 'inactive', merchant_id: @merchant.id)
+  end
 
-    visit '/items'
+  describe "When I visit '/items'" do
+    it 'I see a list of items with their information' do
+      visit '/items'
 
-    expect(page).to have_content('a product')
-    expect(page).to have_content('good product')
-    expect(page).to have_content('Very useful for doing the thing you want')
-    expect(page).to have_content(99.99)
-    expect(page).to have_css("img[src*='https://placehold.it/300x300.jpg']")
-    expect(page).to have_content('active')
-    expect(page).to have_content('inactive')
-    expect(page).to have_content(100)
-    expect(page).to have_content(item1.merchant.name)
+      expect(page).to have_content('a product')
+      expect(page).to have_content('good product')
+      expect(page).to have_content('Very useful for doing the thing you want')
+      expect(page).to have_content(99.99)
+      expect(page).to have_css("img[src*='https://placehold.it/300x300.jpg']")
+      expect(page).to have_content('active')
+      expect(page).to have_content('inactive')
+      expect(page).to have_content(100)
+      expect(page).to have_content(@merchant.name)
+    end
+  end
+
+  describe "When I visit '/merchants/:merchant_id/items'" do
+    it "I see a list of items that belong to the Merchant and their information" do
+      visit "/merchants/#{@merchant.id}/items"
+
+      expect(page).to have_content('a product')
+      expect(page).to have_content('good product')
+      expect(page).to have_content(99.99)
+      expect(page).to have_css("img[src*='https://placehold.it/300x300.jpg']")
+      expect(page).to have_content('active')
+      expect(page).to have_content('inactive')
+      expect(page).to have_content(100)
+    end
   end
 end
