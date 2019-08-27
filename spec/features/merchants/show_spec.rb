f1977f0c8333c8db4669085bcaa2b7d30529a019
NataliaColome <nmcolome@gmail.com>
2019-08-27 13:02:07 -0600

    Add merchant links: merchants#index, items#index & show

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,16 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants show page' do
+  before(:each) do
+    @merchant = create(:merchant)
+    @merchant2 = create(:merchant, name: 'Small Store')
+    @item = create(:item, merchant_id: @merchant.id)
+  end
   it "shows a specific merchant's information" do
-    merchant = create(:merchant)
-    merchant2 = create(:merchant, name: 'Small Store')
+    visit "/merchants/#{@merchant.id}"
 
-    visit "/merchants/#{merchant.id}"
+    expect(page).to have_content(@merchant.name)
+    expect(page).to have_content(@merchant.address)
+    expect(page).to have_content(@merchant.city)
+    expect(page).to have_content(@merchant.state)
+    expect(page).to have_content(@merchant.zip)
+  end
 
-    expect(page).to have_content(merchant.name)
-    expect(page).to have_content(merchant.address)
-    expect(page).to have_content(merchant.city)
-    expect(page).to have_content(merchant.state)
-    expect(page).to have_content(merchant.zip)
+  describe "I visit the merchants index page" do
+    it "When I click on a merchant's name in the merchants index page" do
+      visit '/merchants'
+
+      click_on @merchant.name
+
+      expect(current_path).to eq "/merchants/#{@merchant.id}"
+      expect(page).to have_content(@merchant.address)
+    end
+
+
+    it "When I click on a merchant's name in the items index page" do
+      visit '/items'
+
+      click_on @merchant.name
+
+      expect(current_path).to eq("/merchants/#{@merchant.id}")
+      expect(page).to have_content(@merchant.address)
+    end
+
+    it "When I click on a merchant's name in the items show page" do
+      visit "/items/#{@item.id}"
+
+      click_on @merchant.name
+
+      expect(current_path).to eq("/merchants/#{@merchant.id}")
+      expect(page).to have_content(@merchant.address)
+    end
   end
 end
