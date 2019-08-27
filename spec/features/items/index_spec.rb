17e927193edcf0fe295a411a8d4901346ef4f165
NataliaColome <nmcolome@gmail.com>
2019-08-26 18:13:48 -0600

    Change test to add 'description' in item show page

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -37,7 +37,6 @@ RSpec.describe 'Items index page' do
       expect(page).to have_content('inactive')
       expect(page).to have_content(100)
       expect(page).to_not have_content('other product')
-      expect(page).to_not have_content('Very useful for doing the thing you want')
       expect(page).to_not have_content(@merchant.name)
     end
   end
