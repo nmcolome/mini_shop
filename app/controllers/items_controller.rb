b5c2a61c8e1b6935d9b53138e5b964919afc375f
NataliaColome <nmcolome@gmail.com>
2019-08-27 13:38:27 -0600

    Refactor to single assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    if params[:merchant_id].nil?
-      @items = Item.all
-    else
-      @items = Merchant.find(params[:merchant_id]).items
-    end
+    @items = if params[:merchant_id].nil?
+               Item.all
+             else
+               Merchant.find(params[:merchant_id]).items
+             end
   end
 
   def show
