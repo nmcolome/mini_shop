6cf0c2d9a5236fe7a1b6a3a15861d4c6d8953243
NataliaColome <nmcolome@gmail.com>
2019-08-27 01:30:19 -0600

    Add seed for items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
 require 'faker'
 
-50.times do
-  Merchant.create!(
+20.times do
+  merchant = Merchant.create!(
     name: Faker::Company.name,
     address: Faker::Address.street_address + ' ' + Faker::Address.building_number,
     city: Faker::Address.city,
     state: Faker::Address.state_abbr,
     zip: Faker::Address.zip
   )
+
+  5.times do
+    merchant.items.create!(
+      name: Faker::Food.dish,
+      description: Faker::Food.description,
+      price: Faker::Commerce.price,
+      image: Faker::LoremFlickr.image,
+      inventory: rand(1000)
+    )
+  end
 end
